dd4091c140667a5aede5b25756459f242907d7ba
tristan-gueguen <tristan.gueguen@gmail.com>
2021-02-04 12:28:48 +0100

    removed devServer proxy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   // devServer: {
-  //   // proxy: "https://www.openhumans.org/"
-  //   proxy: "http://localhost:5000/"
+    // proxy: "https://www.openhumans.org/"
+    // proxy: "http://localhost:8083/"
   // }
 };
