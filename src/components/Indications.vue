f94a6b33742acc1dfbd8f9246c95125d7e4c578d
tristan-gueguen <tristan.gueguen@gmail.com>
2021-02-08 11:51:05 +0100

    modified Indications

--- src/components/Indications.vue
+++ src/components/Indications.vue
@@ -1,29 +1,27 @@
 <template>
   <v-container mb-8>
     <v-row justify="center">
-      <!-- <v-col sm="8"> -->
-      <!-- <v-col > -->
-      <v-card color="blue lighten-5" class="py-1 px-6">
+      <v-card color="blue lighten-5" class="py-4 px-10">
+        <slot></slot>
         <!-- <v-card-title class="justify-center">Indications</v-card-title> -->
-        <v-card-text>
-          <ul>
+        <!-- <v-card-text> -->
+          <!-- <ul>
             <li v-for="p in items" :key="p">{{ p }}</li>
-          </ul>
-        </v-card-text>
+          </ul> -->
+        <!-- </v-card-text> -->
       </v-card>
 
-      <!-- </v-col> -->
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  props: {
-    items: {
-      type: Array,
-      required: true,
-    },
-  },
+  // props: {
+  //   items: {
+  //     type: Array,
+  //     required: true,
+  //   },
+  // },
 };
 </script>
